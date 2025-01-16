3dbedbf31ef196a0f80bd753feb990813da199e7
Jorge Luis Perez <jolisper@protonmail.com>
2025-01-16 20:56:57 -0300

    Fix, swap session and user_id keys

--- src/auth-service/sessions.rs
+++ src/auth-service/sessions.rs
@@ -29,16 +29,16 @@ impl Sessions for SessionsTranstient {
         T: Into<String>,
     {
         let session = uuid::Uuid::new_v4().to_string();
-        self.uuid_to_session.insert(user_id.into(), session.clone());
+        self.uuid_to_session.insert(session.clone(), user_id.into());
 
         Ok(session)
     }
 
-    fn delete_session<T>(&mut self, user_id: T) -> Result<(), String>
+    fn delete_session<T>(&mut self, session_token: T) -> Result<(), String>
     where
         T: Into<String>,
     {
-        self.uuid_to_session.remove(&user_id.into());
+        self.uuid_to_session.remove(&session_token.into());
         Ok(())
     }
 }
@@ -55,7 +55,7 @@ mod tests {
         let session = sessions.create_session("1234").unwrap();
 
         assert_eq!(sessions.uuid_to_session.len(), 1);
-        assert_eq!(sessions.uuid_to_session.get("1234").unwrap(), &session);
+        assert_eq!(sessions.uuid_to_session.get(&session).unwrap(), "1234");
     }
 
     #[test]
@@ -63,8 +63,8 @@ mod tests {
         let mut sessions = SessionsTranstient::new();
         assert_eq!(sessions.uuid_to_session.len(), 0);
 
-        sessions.create_session("1234").unwrap();
-        sessions.delete_session("1234").unwrap();
+        let session = sessions.create_session("1234").unwrap();
+        sessions.delete_session(session).unwrap();
 
         assert_eq!(sessions.uuid_to_session.len(), 0);
     }
